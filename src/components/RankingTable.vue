27112947f4010aeb99c7ecf8a139fa1428e94e30
Naotsu86 <p8nk5pny54@privaterelay.appleid.com>
2026-06-28 10:38:34 +0200

    Update RankingTable.vue

--- src/components/RankingTable.vue
+++ src/components/RankingTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ranking-list">
     <article v-for="(r,i) in rows" :key="r.id" class="ranking-row" :class="{ podiumline: i < 3 }">
-      <div class="ranking-place">{{ i + 1 }}</div>
+      <div class="ranking-place">{{ i + 4 }}</div>
       <div class="ranking-name"><b>{{ r.name }}</b><small>{{ r.games }} Spiele · {{ r.wins }} Siege · Ø {{ r.avg }}</small></div>
       <div class="ranking-points">{{ r.points }}</div>
     </article>
